62afc4ee92b76ce3caface5f2d1f5463004071c0
Aniket <aniketkumarpaul1@gmail.com>
2023-01-22 10:35:42 +0530

    Basic setup for react client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      
+    </div>
+  )
+}
+
+export default App
