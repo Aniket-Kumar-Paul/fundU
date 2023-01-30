670e9f1dcc909787869ba20f9a5be3a4a8c8de5a
Aniket <aniketkumarpaul1@gmail.com>
2023-01-30 23:12:05 +0530

    Added Navbar and extra routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,11 +9,15 @@ const App = () => {
       <div className='sm:flex hidden mr-10 relative'>
         <Sidebar />
       </div>
-      
+
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
         <Navbar />
         <Routes>
           <Route path='/' eljement={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-campaign' element={<CreateCampaign />} />
+          {/* Dynamic Route */}
+          <Route path='/campaign-details/:id' element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
